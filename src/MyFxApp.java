f72bc3220c5a27fc09ee2f09f3a79dd4e0e5f41a
jadenwilliams <jaw98x@gmail.com>
2023-11-21 15:03:06 -0500

    ghh

--- src/MyFxApp.java
+++ src/MyFxApp.java
@@ -101,7 +101,7 @@ public class MyFxApp extends Application {
         Scene myScene1 = new Scene(vbox1, 400, 500);
 //kk
         // setup scene on the stage and show it
-        primaryStage.setTitle("Image Picker");
+        primaryStage.setTitle("Image Picker");  // mr h
         primaryStage.setScene(myScene1);
         primaryStage.show();
 
